c98f76b637800ebaf0009bd70264a9a7b75277e2
BuildTools <ama84472@gmail.com>
2018-08-18 23:05:19 -0400

    Wrote a quick plan for a teleport command. Did some studying up on Bukkit API

---
+++ Plugins 1/src/com/gmail/ama84472/PluginTest/TestPlugin.java
@@ -0,0 +1,20 @@
+package com.gmail.ama84472.PluginTest;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import commands.TestCommand;
+
+public class TestPlugin extends JavaPlugin{
+
+	public void onEnable() {
+		
+		System.out.println("Test Plugin Initialized");
+		this.getCommand("commandTest").setExecutor((CommandExecutor)new TestCommand());
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
---
+++ Plugins 1/src/commands/Warp.java
@@ -0,0 +1,22 @@
+package commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Warp implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		Player player = (Player)sender;
+		
+		
+		return true;
+	}
+
+	
+	
+	
+}
